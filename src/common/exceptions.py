d57b53cdd393e2e8d4df574637cb430425a1be0d
Pavel Khramov <102523130+KhramovKhramov@users.noreply.github.com>
2025-09-17 22:11:10 +0500

    Project initial commit (#1)

---
+++ src/common/exceptions.py
@@ -0,0 +1,15 @@
+from fastapi import status
+from fastapi.responses import JSONResponse
+
+
+def sqlalchemy_not_found_exception_handler(*_):
+    """
+    Обработчик стандартного 404-случая, когда объект не найден в БД.
+    """
+
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={
+            'detail': 'Объект с таким идентификатором не найден',
+        },
+    )
